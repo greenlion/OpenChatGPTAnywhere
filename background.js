5bc27bca603770ff965dabea52bc6a02438bd211
Justin Swanhart <greenlion@gmail.com>
2023-03-28 06:19:25 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,27 +1,3 @@
-    /*async function fetchGPTResponse(query) {
-      console.error('here4');
-      const openai = require('openai');
-      openai.apiKey = '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';
-
-      try {
-        const result = await openai.Completion.create({
-          engine: 'text-davinci-002',
-          prompt: query,
-          max_tokens: 100,
-          n: 1,
-          stop: null,
-          temperature: 1,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        });
-
-        return result.choices[0].text.trim();
-      } catch (error) {
-        console.error('Error fetching ChatGPT response:', error);
-        return 'Error: Unable to process query.';
-      }
-    }*/
     async function fetchGPTResponse(query) {
       
       const apiKey = 'your-api-key-here';
